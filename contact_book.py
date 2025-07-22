ecedd4888891eed789c741d366f1c1fb7c7df8a6
Uzo-codes <uzochichinedu100@gmail.com>
2025-07-22 12:23:12 +0100

    Updated code

--- contact_book.py
+++ contact_book.py
@@ -1,14 +1,19 @@
+#Initialize an empty dictionary to store contact names and numbers
 contacts = {}
 
+#Start an infinite loop to repeatedly prompt the user for actions
 while True:
+    #User chooses an option
     action = input("Choose: add / view / exit: ")
 
     if action == "add":
+        #Request for contact name and number, then add to the dictionary
         name = input("Name: ")
         number = int(input("Number: "))
         contacts[name] = number
 
     elif action == "view":
+        #Displays all the saved contacts
         for name, number in contacts.items():
             print(f"{name}: {number}")
 
